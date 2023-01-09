e4f0b1bf4808ae0cb7789bfd5b06a14528d8e74f
mfndevil <71969249+mfndevil@users.noreply.github.com>
2023-01-09 20:52:37 +0000

    Added comments to explain code

--- ComputerVision.py
+++ ComputerVision.py
@@ -13,19 +13,29 @@ video = cv2.VideoCapture(0)
 
 # Runs program on a loop 
 while True:
+
+    # Reads webcam footage
     success, img = video.read()
+
+    # Converts BGR to HSV 
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Finding contours in mask image
     mask = cv2.inRange(image, lower, upper)
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Finds position of all contours 
     if len(contours) != 0:
         for contour in contours:
             if cv2.contourArea(contour) > 500:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(img, (x,y), (x + w, y +h), (0, 0, 255), 3)  
 
+    # Display mask image
     cv2.imshow("mask", mask)
+
+    # Display webcam footage
     cv2.imshow("webcam", img)
 
     cv2.waitKey(1)
